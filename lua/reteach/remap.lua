f75512722088007b5100a2ff0ebacbc41292d080
AJ Steinhauser <aj.steinhauser@perficient.com>
2023-07-20 16:27:26 -0400

    Null registers

--- lua/reteach/remap.lua
+++ lua/reteach/remap.lua
@@ -12,10 +12,22 @@ vim.keymap.set("n", "<leader>pq", ":q<enter>")
 vim.keymap.set("n", "<leader>pt", ":Fern .<enter>")
 vim.keymap.set("n", "<leader>pr", ":Fern . -reveal=%<enter>")
 
+--Terminal popup
 vim.keymap.set("n", "<leader>tt", ":ToggleTerm direction=float<enter>")
+
 --Autocomplete remaps
 vim.keymap.set("n", "<leader>y", "<C>y")
 
 
+--Trouble menu popup
 vim.keymap.set("n", "<leader>t", ":Trouble<enter>")
 vim.keymap.set("n", "<leader>T", ":TroubleClose<enter>")
+
+
+--Null Registers 
+vim.keymap.set("n", "x", "\"_x")
+vim.keymap.set("n", "<leader>d", "\"_d")
+
+
+
+
